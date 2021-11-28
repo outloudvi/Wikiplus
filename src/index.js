d3b8db8b5049176bf5336da967d7e134d65dd1f2
Outvi V <19144373+outloudvi@users.noreply.github.com>
2021-11-28 05:59:36 +0000

    fix: fix loading
    seems that jQuery(cb) does not accept Promise?

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ import Constants from "./utils/constants";
 import i18n from "./utils/i18n";
 import "./wikiplus.css";
 
-$(async () => {
+const loadWikiplus = async () => {
     const Pages = {};
     const isNewPage = $(".noarticletext").length > 0 && Constants.articleId === 0;
 
@@ -181,4 +181,8 @@ $(async () => {
     UI.insertSimpleRedirectButton(handleSimpleRedirectButtonClicked);
     UI.insertSettingsPanelButton(handleSettingsButtonClicked);
     UI.bindPreloadEvents(handlePreload);
+};
+
+$(() => {
+    loadWikiplus();
 });
